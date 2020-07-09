5e710895c578221ad98c98ba224a07d969c7851e
yyash01 <yjain3.436@gmail.com>
2020-07-10 01:20:07 +0530

    added-calculator

---
+++ calculator/script.js
@@ -0,0 +1,9 @@
+function getHistory(){
+	return document.getElementById("history-value").innerText;
+}
+function printHistory(num){
+	document.getElementById("history-value").innerText=num;
+}
+function getOutput(){
+	return document.getElementById("output-value").innerText;
+}
